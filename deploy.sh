6500a7cafc611dafa7777c74f7ea299eb7019120
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-07-05 15:30:32 +0430

    updated deploy.sh_6

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 cp .env.example .env
-sudo chmod -R 775 ./bootstrap/cache
-sudo chmod -R 775 ./storage
-sudo chown -R $USER:www-data ./storage
-sudo chown -R $USER:www-data ./bootstrap/cache
+#sudo chmod -R 775 ./bootstrap/cache
+#sudo chmod -R 775 ./storage
+#sudo chown -R $USER:www-data ./storage
+#sudo chown -R $USER:www-data ./bootstrap/cache
 composer install --optimize-autoloader --no-dev
 php artisan key:generateg
 php artisan config:cache

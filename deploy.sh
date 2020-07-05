6380c3273378900ea5ee10147bc53e89bae58061
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-07-05 15:48:32 +0430

    updated deploy.sh_10

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 cp .env.example .env
 chmod -R 775 storage bootstrap/cache
-chown chown -R $USER:www-data storage bootstrap/cache
+chown -R $USER:www-data storage bootstrap/cache
 composer install --optimize-autoloader --no-dev
 php artisan key:generate
 php artisan config:cache

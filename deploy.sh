143eac14e0ee45b0e5a4920b4d6410c15242e1bc
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-07-05 17:16:22 +0430

    added checking env file existence2

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
-
 FILE=/home/maqsudi/workspace/larakins/.env
 if [ -f $FILE ]; then
     echo "$FILE exists"
 else
-    echo "$FILE does not exists"
+    cp .env.example .env
+    php artisan key:generate
 fi
+
 #chmod -R 775 storage bootstrap/cache
 #chown -R $USER:www-data storage bootstrap/cache
-#cp .env.example .env
 composer install --optimize-autoloader --no-dev
-#php artisan key:generate
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache

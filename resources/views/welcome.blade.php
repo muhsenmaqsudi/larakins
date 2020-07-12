9e8f8b1322657682b205391ba564fa3a0e2e3421
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-07-12 16:51:31 +0430

    testing ci/cd live 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Testing CI/CD 2
+                    Laravel Testing CI/CD 3
                 </div>
 
                 <div class="links">
